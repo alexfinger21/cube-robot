6dddb56f848254d87cd58c7c76549bc8df88d236
shmurthy7002 <yhtrum7002@gmail.com>
2023-08-18 23:15:11 -0400

    fire color detection 💦💦😎😎😎

--- getCubeState.py
+++ getCubeState.py
@@ -2,11 +2,11 @@ import cv2
 import numpy as np
 import constants
 
-
 global uBGR, dBGR, rBGR, lBGR, fBGR, bBGR
 
 bgr_vals = []
 cube_state_string = ""
+
 
 def getImage(camera):
     if camera == 1:
@@ -36,29 +36,44 @@ def euclidean_distance(color1, color2):
     return np.sqrt(np.sum((color1 - color2) ** 2))
 
 
-def findClosestCenterFacelet():
-    pass
+def findClosestCenterFacelet(color):
+    global uBGR, rBGR, fBGR, lBGR, dBGR, dBGR
+
+    distances_dict = {euclidean_distance(color, uBGR): "U", euclidean_distance(color, rBGR): "R",
+                      euclidean_distance(color, fBGR): "F", euclidean_distance(color, dBGR): "D",
+                      euclidean_distance(color, lBGR) : "L", euclidean_distance(color, bBGR) : "B"}
+    distances_list = [euclidean_distance(color, uBGR), euclidean_distance(color, rBGR), euclidean_distance(color, fBGR),
+                      euclidean_distance(color, dBGR), euclidean_distance(color, lBGR)]
+    return distances_dict.get(min(distances_list))
 
 
-image = getImage(1)
-cv2.imshow("image", image)
+imageCam1 = getImage(1)
+imageCam2 = getImage(2)
 
-for facelet in constants.FACELETS:
-    bgr_vals.append(getBGR(facelet, image))
+cv2.imshow("image", imageCam1)
 
-uBGR = getBGR(constants.FACELETS[4], image)
-rBGR = getBGR(constants.FACELETS[13], image)
-fBGR = getBGR(constants.FACELETS[22], image)
-dBGR = getBGR(constants.FACELETS[31], image)
-lBGR = getBGR(constants.FACELETS[40], image)
-bBGR = getBGR(constants.FACELETS[49], image)
+for facelet in constants.FACELETS_CAM_1:
+    bgr_vals.append(getBGR(facelet, imageCam1))
 
+for facelet in constants.FACELETS_CAM_2:
+    bgr_vals.append(getBGR(facelet, imageCam2))
 
+# Gets all the center facelet BGR values
+uBGR = getBGR(constants.FACELETS_CAM_1[4], imageCam1)
+rBGR = getBGR(constants.FACELETS_CAM_1[13], imageCam1)
+fBGR = getBGR(constants.FACELETS_CAM_1[22], imageCam1)
+dBGR = getBGR(constants.FACELETS_CAM_2[4], imageCam2)
+lBGR = getBGR(constants.FACELETS_CAM_2[13], imageCam2)
+bBGR = getBGR(constants.FACELETS_CAM_2[22], imageCam2)
 
+bgr_vals = np.array(bgr_vals)
 
+for val in bgr_vals:
+    cube_state_string += findClosestCenterFacelet(val)
+
+print(cube_state_string)
 while True:
     key = cv2.waitKey(1)
 
     if key == 27:  # Press ESC key to exit
         break
-
